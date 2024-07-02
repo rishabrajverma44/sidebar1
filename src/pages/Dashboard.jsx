1c4995061026ed3e3aa27dcb0512617e4bd5bde5
Lahiru <gdayananda@zaion.ai>
2024-07-02 11:11:07 +0200

    commit full project

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Analytics from "./Analytics";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>dashboard page</h1>
+      <Analytics />
+    </div>
+  );
+};
+
+export default Dashboard;
